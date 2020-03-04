db2dc8655cee6cc08a4ddefb1941317144c9398c
souru98 <kulkarnisourabh1998@gmail.com>
2020-03-05 01:01:31 +0530

    my code

--- src/services/Booking.java
+++ src/services/Booking.java
@@ -2,3 +2,16 @@ package services;
 
 // Type your code
 
+//parent class
+public class Booking{
+	
+	//method returning rates
+	public double booking(int noOfPersons,int rates)
+	{
+	
+		return noOfPersons*rates;
+		
+}
+	
+	
+}
