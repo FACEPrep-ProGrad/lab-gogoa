00d9153990419ba1ed6305c7e3b8167ff322359e
NARNEHARIPRIYA <61201057+NARNEHARIPRIYA@users.noreply.github.com>
2020-03-05 11:27:54 +0530

    modified

--- src/controller/BusController.java
+++ src/controller/BusController.java
@@ -9,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Bus;
+import services.FareCalculator;
 
 
 /**
@@ -32,7 +34,7 @@ public class BusController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*
+
 		
 			int numberOfPersons=Integer.parseInt(request.getParameter("persons"));
 		String bustype=request.getParameter("bustype");
@@ -76,6 +78,6 @@ public class BusController extends HttpServlet {
 		request.setAttribute("busfare", rate);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/busView.jsp");
 		dispatcher.forward(request, response);
-*/	}
+	}
 	
 }
