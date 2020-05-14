f8aa3de0a4c230ebb8d6c4a059e57b15bb5b9cc9
Varsha <hp@DESKTOP-4IENAI5>
2020-05-14 11:52:04 +0530

    FSJ0420A113

--- src/controller/BusController.java
+++ src/controller/BusController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import services.*;
 import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
@@ -9,7 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import model.*;
 @WebServlet(urlPatterns= {"/bus"})
 public class BusController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -28,7 +29,7 @@ public class BusController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*
+
 		
 			int numberOfPersons=Integer.parseInt(request.getParameter("persons"));
 		String bustype=request.getParameter("bustype");
@@ -72,6 +73,6 @@ public class BusController extends HttpServlet {
 		request.setAttribute("busfare", rate);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/busView.jsp");
 		dispatcher.forward(request, response);
-*/	}
+	}
 	
 }
