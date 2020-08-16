daa17230aff45781ec43a686c8031fb9d6345771
GurramkondaYaswanth <gurramkondayaswanth@gmail.com>
2020-08-16 18:58:40 +0530

    FSJ0620C106

--- src/services/Booking.java
+++ src/services/Booking.java
@@ -1,4 +1,11 @@
 package services;
 
 // Type your code
-
+//class called as Booking
+public class Booking{
+	//method public double booking(int noOfPersons,int rates).
+	public double booking(int noOfPersons,int rates) {
+		double totalRate =noOfPersons*rates;
+		return totalRate;		
+	}
+}
